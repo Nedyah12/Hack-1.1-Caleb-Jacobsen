3fd38183d65aad815676942b80cafc82a15e2d28
Calebj1 <42581033+Calebj1@users.noreply.github.com>
2018-08-23 11:50:26 -0500

    hello .c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Caleb jacobsen
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Caleb Jacobsen\n");
+
+  return 0;
+}
